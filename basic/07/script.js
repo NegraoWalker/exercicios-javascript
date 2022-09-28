ff1b3fc5a00793f4794096dd0644c877b577a369
walkerNegrão <walker.esteves.negrao@gmail.com>
2022-09-28 19:41:00 -0300

    Exercicio 7 - Finalizado

---
+++ basic/07/script.js
@@ -0,0 +1,6 @@
+/*Exiba a potênciação dos números 2,3,18 e 0 com expoente 2: */
+
+console.log(Math.pow(2, 2))
+console.log(Math.pow(2, 3))
+console.log(Math.pow(2, 18))
+console.log(Math.pow(2, 0))
